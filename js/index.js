e399ec9ea2ef73531b0c737fe940777543d52224
John O'Connor <oconnojb@gmail.com>
2018-10-21 20:32:48 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // use this to copy code snippets or us your browser's console
+function greet() {
+	console.log(`my name is ${this.name}, hi!`);
+}
+
+let sally = { name: 'Sally' };
+
+greet.call(sally);
+// my name is Sally, hi!
+
+greet.apply(sally);
+// my name is Sally, hi!
