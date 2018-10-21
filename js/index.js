7bdeec40c83941a2484784319eab048cb4ade65e
John O'Connor <oconnojb@gmail.com>
2018-10-21 20:41:51 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// use this to copy code snippets or us your browser's console
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+	console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+greet.bind(sally)('Bob');
+// Hi Bob, my name is Sally!
